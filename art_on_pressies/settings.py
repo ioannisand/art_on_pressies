12c0a93e1cb3750316b955e3ae1fa25dd08bd2d7
AndreaIo <ioannis.andreadakis@partners.basf.com>
2026-04-13 17:15:43 +0300

    Fix CSRF trusted origins for Fly.io HTTPS

--- art_on_pressies/settings.py
+++ art_on_pressies/settings.py
@@ -26,6 +26,8 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')
+
 # Base directory for persistent data (DB + media). Override in production via DATA_DIR env var.
 DATA_DIR = Path(os.environ.get('DATA_DIR', BASE_DIR))
 
