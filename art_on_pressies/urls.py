a297400970ed200b0051fec5f4e2f5c6cdfcbba2
AndreaIo <ioannis.andreadakis@partners.basf.com>
2026-04-13 18:15:09 +0300

    Replace static() with direct media serving view

--- art_on_pressies/urls.py
+++ art_on_pressies/urls.py
@@ -1,11 +1,22 @@
+import os
+
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import FileResponse, Http404
 from django.urls import include, path
+from django.views.decorators.cache import never_cache
+
+
+@never_cache
+def serve_media(request, path):
+    full_path = os.path.join(str(settings.MEDIA_ROOT), path)
+    if os.path.isfile(full_path):
+        return FileResponse(open(full_path, 'rb'))
+    raise Http404()
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('media/<path:path>', serve_media),
     path('', include('core.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
